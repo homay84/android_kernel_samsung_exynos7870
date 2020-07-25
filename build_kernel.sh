379387775a42a28af751f790549e074feeed45ad
Pand44X <blogrpcristo@gmail.com>
2020-07-25 15:47:00 +0000

    build_kernel.sh

---
+++ build_kernel.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export ARCH=arm64
+export CROSS_COMPILE=/android/toolchain/gcc-linaro-7.5.0/bin/aarch64-linux-gnu-
+export ANDROID_MAJOR_VERSION=q
+export ANDROID_PLATFORM_VERSION=10
+
+make exynos7870-j7y17lte_defconfig
+make -j64
